b7d8eb7274551ba5fcd5f4726893e6302de4a155
ApplyYoon <126080767+ApplyYoon@users.noreply.github.com>
2025-07-11 14:05:45 +0900

    Add files via upload

---
+++ src/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { questions } from './data/questions';
+import Question from './components/Question';
+import Result from './components/Result';
+import './App.css';
+
+function App() {
+  const [step, setStep] = useState(0);
+  const [scores, setScores] = useState({
+    fruit: 0, flower: 0, perfume: 0, nature: 0, powder: 0, iff: 0, france: 0
+  });
+
+  
+  const handleAnswer = (types) => {
+    const newScores = { ...scores };
+    types.forEach(type => newScores[type]++);
+    setScores(newScores);
+    setStep(step + 1);
+  };
+
+  return (
+    <div className="app">
+      {step < questions.length ? (
+        <Question key={step} data={questions[step]} onAnswer={handleAnswer} />
+      ) : (
+        <Result scores={scores} />
+      )}
+    </div>
+  );
+}
+
+export default App;
